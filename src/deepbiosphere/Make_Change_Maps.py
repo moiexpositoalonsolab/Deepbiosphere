554efbb5cf074dc95654c26f2482217b7372bdd9
Lauren Gillespie <leg2015@gmail.com>
2025-09-15 13:08:07 -0700

    including bioclim raster glob path when useful

--- src/deepbiosphere/Make_Change_Maps.py
+++ src/deepbiosphere/Make_Change_Maps.py
@@ -7,6 +7,8 @@ import deepbiosphere.Run as run
 import deepbiosphere.Utils as utils
 import deepbiosphere.Models as mods
 from deepbiosphere.Utils import paths
+from deepbiosphere.Losses import Loss as losses
+from deepbiosphere.Models import Model as mods
 import deepbiosphere.NAIP_Utils as naip
 import deepbiosphere.Build_Data as build
 import deepbiosphere.Make_Maps as maps
@@ -138,7 +140,7 @@ def predict_raster_intime(pred_outline : gpd.GeoDataFrame,
                         sat_res : int = 1.0, # resolution to upsample sat imagery to, in meters
                         impute_climate = True,
                         generate_preds=False,
-                        end_dir = None):
+                        bioclim_train_dir=paths.RASTERS):
     if generate_preds:
         # get climate data first
         clim_rasters = build.get_bioclim_rasters(state=state)
@@ -161,7 +163,8 @@ def predict_raster_intime(pred_outline : gpd.GeoDataFrame,
                                     pred_res=pred_res, # meter resolution of predictions to make with DBS
                                     sat_res=sat_res, # resolution to upsample sat imagery to
                                     impute_climate=impute_climate,
-                                    clim_rasters = clim_rasters)
+                                    clim_rasters = clim_rasters,
+                                    train_dir=bioclim_train_dir)
         # 2. predict rasters in end_year
         end_ras = maps.predict_rasters_list(pred_outline=pred_outline,
                                     pred_types=pred_types,
@@ -178,7 +181,9 @@ def predict_raster_intime(pred_outline : gpd.GeoDataFrame,
                                     pred_res=pred_res, # meter resolution of predictions to make with DBS
                                     sat_res=sat_res, # resolution to upsample sat imagery to
                                     impute_climate=impute_climate,
-                                    clim_rasters = clim_rasters)
+                                    clim_rasters = clim_rasters,
+                                    train_dir=bioclim_train_dir)
+
     # 3. calculate change between the years
     start_ras = glob.glob(f"{paths.RASTERS}{start_dir}/{pred_res}m_{start_year}_{band}_{cfg.exp_id}_{epoch}/*/*raw*.tif")
     end_ras = glob.glob(f"{paths.RASTERS}{end_dir}/{pred_res}m_{end_year}_{band}_{cfg.exp_id}_{epoch}/*/*raw*.tif")
@@ -213,8 +218,8 @@ if __name__ == "__main__":
     args.add_argument('--end_year', type=int, help='What year of imagery to make after prediction on', default=2014)
     args.add_argument('--exp_id', type=str, help='Experiment ID for model to use for mapmaking', required=True)
     args.add_argument('--band', type=str, help='Band which model to use for mapmaking was trained on', required=True)
-    args.add_argument('--loss', type=str, help='Loss function used to train mapmaking model', required=True, choices=run.LOSSES.keys())
-    args.add_argument('--architecture', type=str, help='Architecture of mapmaking model', required=True, choices=run.MODELS.keys())
+    args.add_argument('--loss', type=str, help='Loss function used to train mapmaking model', required=True, choices=losses.valid())
+    args.add_argument('--architecture', type=str, help='Architecture of mapmaking model', required=True, choices=mods.valid())
     args.add_argument('--epoch', type=int, help='what model epoch to use for making maps', required=True)
     args.add_argument('--batch_size', type=int, help='what size batch to use for making map inference', required=True)
     args.add_argument('--pred_resolution', type=int, help='what meter resolution to make map', default=utils.IMG_SIZE)
@@ -223,6 +228,7 @@ if __name__ == "__main__":
     args.add_argument('--device', type=int, help="Which CUDA device to use. Set -1 for CPU", default=-1)
     args.add_argument('--state', type=str, help='What state predictions are being made int', default='ca')
     args.add_argument('--alpha_type', type = str, help='What type of alpha prediction to make', choices = naip.Alpha.valid(), default='SUM')
+    args.add_argument('--bioclim_train_dir', type=str, help='Location of bioclim rasters used in training if different from inference time', default=paths.RASTERS)
     args.add_argument('--processes', type=int, help="How many worker processes to use for mapmaking", default=1)
     args.add_argument('--impute_climate', action='store_true', help="whether to impute the climate for locations with no bioclim coverage")
     args.add_argument('--generate_preds', action='store_true', help="whether to generate predictions for both years if not already done")
@@ -257,5 +263,6 @@ if __name__ == "__main__":
                         change_fn = args.change_fn,
                         sat_res = args.sat_resolution,
                         impute_climate = args.impute_climate,
-                        generate_preds = args.generate_preds)
+                        generate_preds = args.generate_preds,
+                        bioclim_train_dir = args.bioclim_train_dir)
 
